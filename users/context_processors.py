c82fdaf213225fd4579837bfd1021932538989ae
hunghnuit <hunghn0999@gmail.com>
2020-06-25 09:07:33 +0700

    Remove db, add authenticate user for number item in cart

--- users/context_processors.py
+++ users/context_processors.py
@@ -1,6 +1,9 @@
 from users.models import ItemSelection
 def add_variable_to_context(request):
-    number_item_in_cart = sum([item.quantity for item in ItemSelection.objects.filter(user=request.user,ordered=False)])
+    number_item_in_cart = 0
+    if request.user.is_authenticated:
+        number_item_in_cart = sum([item.quantity for item in ItemSelection.objects.filter(user=request.user,ordered=False)])
     return {
         'number_item_in_cart': number_item_in_cart
     }
+    
