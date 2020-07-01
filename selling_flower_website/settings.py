25f179b725ffbd61b2f9d39608ad14a0a3071640
Phuc Nguyen <nhp200199@gmail.com>
2020-07-01 22:53:47 +0700

    add ALLOWED_HOSTS

--- selling_flower_website/settings.py
+++ selling_flower_website/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'h*0)%)do=zaugf(=lw+g%*--z#z7tphxn52aai81$^=ti)(t7a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tubaflowers.herokuapp.com']
 
 
 # Application definition
