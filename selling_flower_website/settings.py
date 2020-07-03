54aad097df22e2c7c735d5515c2294391a38b5ac
Phuc Nguyen <nhp200199@gmail.com>
2020-07-04 00:31:06 +0700

    tried to update settings to include static files

--- selling_flower_website/settings.py
+++ selling_flower_website/settings.py
@@ -136,7 +136,8 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
+STATIC_URL = 'https://s3.amazonaws.com/%s' % os.environ.get('AWS_STORAGE_STATIC_FILES') + '/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'flowerweb-official'),
 ]
@@ -214,9 +215,11 @@ CKEDITOR_UPLOAD_PATH = "ckeditor-upload/"
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_STORAGE_STATIC_FILES = os.environ.get('AWS_STORAGE_STATIC_FILES')
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_REGION_NAME = 'us-east-2'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
