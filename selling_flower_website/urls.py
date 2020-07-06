430424dded5972dd10e5a93c02865402c4934fb3
hunghnuit <hunghn0999@gmail.com>
2020-07-06 10:31:49 +0700

    A small change for static root.

--- selling_flower_website/urls.py
+++ selling_flower_website/urls.py
@@ -62,7 +62,7 @@ urlpatterns = [
     # path('proceed/',users_views.loadcheckout,name='checkout')
     # Test
     # path('test/',users_views.response_api,name='test')
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
 
 if settings.DEBUG:
     # Thịnh's hint for uploading heroku.
